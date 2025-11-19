3717ef910b2579043900c0d20766f6c2ef261e92
newton <newtonoyagha@gmail.com>
2025-11-19 20:18:26 +0100

    corrected route

--- libs/utils.js
+++ libs/utils.js
@@ -29,7 +29,7 @@ export function setToken(userid, res) {
   res.cookie("token", token, {
     httpOnly: true,
     secure: true,
-    sameSite: "lax",
+    sameSite: "none",
      
     maxAge: 7 * 24 * 60 * 60 * 1000,
     path: "/", 
