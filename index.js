eaa32a9a880eb3a8755c7cf6175a58b1e6bc4883
newton <newtonoyagha@gmail.com>
2025-11-19 20:38:34 +0100

    corrected route

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const CLIENT_URL =process.env.CLIENT_URL || "http://localhost:5173"
 
 
 app.use(cors({
-  origin:CLIENT_URL,
+  origin:"https://brainseek.vercel.app/",
   credentials: true,
 }))
 
