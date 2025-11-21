31f07deeb3dddcdcdf82a91b2e5118e924bd32a6
newton <newtonoyagha@gmail.com>
2025-11-21 10:22:16 +0100

    added sub domain cookie handler

--- libs/utils.js
+++ libs/utils.js
@@ -2,39 +2,22 @@ import jwt from "jsonwebtoken"
 import dotenv from "dotenv"
 dotenv.config();
 
-
-/*export function setToken(userid, res) {
-      const token = jwt.sign({id:userid}, process.env.SESSION_SECRET, {expiresIn: '7d'})
-
-      res.cookie("token", token, {
-            httpOnly: true,
-            secure:process.env.NODE_ENV === "production"? true : false,
-            sameSite: "None",
-            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-      })
-
-      
-}*/
-
-
-
-
-
-
-
 export function setToken(userid, res) {
   const token = jwt.sign({ id: userid }, process.env.SESSION_SECRET, { expiresIn: "7d" });
   const isProduction = process.env.NODE_ENV === "production";
   
-  res.cookie("token", token, {
+  const cookieOptions = {
     httpOnly: true,
-    secure: true,
-    sameSite: "None",
-     
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    path: "/", 
-    
-  });
+    secure: true, 
+    sameSite: "None", 
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    path: "/",
+  };
+
+  
+  if (isProduction) {
+    cookieOptions.domain = ".vercel.app"; 
+  }
+  
+  res.cookie("token", token, cookieOptions);
 }
-
-
